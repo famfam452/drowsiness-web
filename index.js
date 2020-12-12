000c4a822450439066432b4aa1e957de4d46478b
sMARCHz <mart_nattanon@hotmail.com>
2020-12-12 21:24:31 +0700

    refactor

--- index.js
+++ index.js
@@ -14,16 +14,17 @@ app.get("/", function (req, res) {
 });
 
 socket.on("connection", function (ws, req) {
+  console.log("new client connected");
   ws.on("message", function (message) {
     socket.clients.forEach(function (client) {
       //broadcast incoming message to all clients (s.clients)
       if (client != ws && client.readyState) {
         //except to the same client (ws) that sent this message
         client.send("broadcast: " + message);
+        console.log('sending...')
       }
     });
   });
-  console.log("new client connected");
 });
 
 server.listen(PORT);
